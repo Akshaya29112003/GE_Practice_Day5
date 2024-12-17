cfba4efc8f4c68a990adc3e91388950bbf31eae6
Akshaya <nalamakshaya04@gmail.com>
2024-12-18 00:05:37 +0530

    Add: Contact Details Implementation

---
+++ Address_Book_System/ContactPerson.java
@@ -0,0 +1,44 @@
+package Address_Book_System;
+
+public class ContactPerson {
+    public String fname;
+    public String lname;
+    public String address;
+    public String city;
+    public String state;
+    public String zip;
+    public String phone;
+    public String email;
+
+    public ContactPerson(String fname, String lname, String address, String city, String state, String zip, String phone, String email) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+    public String getFname(){
+        return fname;
+    }
+
+    public String getLname(){
+        return lname;
+    }
+
+    public void updateDetails(String address, String city, String state, String zip, String phone, String email){
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        return "Name: " + fname + " " + lname + "\nAddress: " + address + ", " + city + ", " + state + ", " + zip + "\nPhone: " + phone + "\nEmail: " + email;
+    }
+}
